eae70eb4138ee3e68a8ce2d48530614f987fdce2
Nithish-Kumar-MT <nithishswim7@gmail.com>
2024-11-13 12:21:39 +0530

    user session

--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";    
 $password = "";    
-$dbname = "deadstock";  
+$dbname = "deadstock_db";  
 
 // MySQLi Connection
 $conn = new mysqli($servername, $username, $password, $dbname);
