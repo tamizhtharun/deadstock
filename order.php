9a4069a5c94b234ae09d3e137037143658cdf067
tamizhtharun <mailtotharun23@gmail.com>
2025-01-12 18:32:30 +0530

    qq

--- order.php
+++ order.php
@@ -1,1 +1,9 @@
-<?php include('db_connection.php'); ?>
+<?php include('db_connection.php'); 
+
+if (!isset($_SESSION['user_session']['id'])) {
+    header('Location: login.php');
+    exit;
+}
+
+$user_id = $_SESSION['user_session']['id'];
+
