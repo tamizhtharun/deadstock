dfd3e6247eefa6b14de32332f6a63bc0af3b7fc0
NithishKumar <nithishkumar2320005@gmail.com>
2025-01-27 12:15:30 +0530

    Seller dashboard

--- cart.php
+++ cart.php
@@ -3,7 +3,7 @@ include 'db_connection.php';
 include 'header.php';
 
 if (!isset($_SESSION['user_session']['id'])) {
-    header('Location: login.php');
+    header('Location: index.php');
     exit;
 }
 
@@ -28,19 +28,11 @@ if (isset($_GET['delete_all'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shopping Cart | Your Store</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/cart.css">
-</head>
 
-<body>
     <div class="cart-page">
         <div class="container py-5">
             <!-- Cart Header -->
@@ -269,6 +261,3 @@ if (isset($_GET['delete_all'])) {
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/cart.js"></script>
-</body>
-
-</html>
