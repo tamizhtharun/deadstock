44f2045783c4069e88264535737278bbc7039b93
tamizhtharun <mailtotharun23@gmail.com>
2025-01-29 23:29:19 +0530

    123

--- header.php
+++ header.php
@@ -46,7 +46,6 @@ unset($_SESSION['error_message']);
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="./css/index.css">
-    <link rel="stylesheet" href="./css/responsive.css">
     <link rel="stylesheet" href="./css/header.css">
     <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
 
