6b7e0fe2b55776e32dbf6ba61ae434ca9c76bf67
tamizhtharun <mailtotharun23@gmail.com>
2025-12-02 14:43:35 +0530

    product-add.php bug fixed

--- seller/get-mid-category.php
+++ seller/get-mid-category.php
@@ -1,16 +1,28 @@
 <?php
+// Simple test endpoint for debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 include '../db_connection.php';
-if($_POST['id'])
-{
-	$id = $_POST['id'];
-	
-	$statement = $pdo->prepare("SELECT * FROM tbl_mid_category WHERE tcat_id=?");
-	$statement->execute(array($id));
-	$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-	?><option value="">Select Mid Level Category</option><?php						
-	foreach ($result as $row) {
-		?>
-        <option value="<?php echo $row['mcat_id']; ?>"><?php echo $row['mcat_name']; ?></option>
-        <?php
-	}
+
+header('Content-Type: text/html; charset=utf-8');
+
+if(isset($_POST['id'])) {
+    $id = $_POST['id'];
+    
+    try {
+        $statement = $pdo->prepare("SELECT * FROM tbl_mid_category WHERE tcat_id=?");
+        $statement->execute(array($id));
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        
+        echo '<option value="">Select Mid Level Category</option>';
+        foreach ($result as $row) {
+            echo '<option value="' . htmlspecialchars($row['mcat_id']) . '">' . htmlspecialchars($row['mcat_name']) . '</option>';
+        }
+    } catch(PDOException $e) {
+        echo '<option value="">Error: ' . htmlspecialchars($e->getMessage()) . '</option>';
+    }
+} else {
+    echo '<option value="">No ID provided</option>';
 }
+?>
