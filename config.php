883ffaac6e8377077223f2e6535ca357d445ca46
tamizhtharun <mailtotharun23@gmail.com>
2025-12-03 22:06:16 +0530

    razorpay api

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-define('RAZORPAY_KEY_ID', 'rzp_test_a2BAu1z7bW12nc');      // Test Key ID
-define('RAZORPAY_KEY_SECRET', 'jPH4B7E5pKOwlbLa9gawsb7Q');  // Test Key Secret
+define('RAZORPAY_KEY_ID', 'rzp_test_RnBSe1L1ROHN0V');      // Test Key ID
+define('RAZORPAY_KEY_SECRET', 'kfATzwv65lFnRnOY4aGK4YRq');  // Test Key Secret
 ?>
