c296007ee07ae990de15afb90e07f5e422e71c30
tamizhtharun <mailtotharun23@gmail.com>
2025-12-04 17:24:54 +0530

    index page - product image update

--- index.php
+++ index.php
@@ -399,11 +399,11 @@ if (!empty($topCategories)) {
 
                     .product-img {
                         position: absolute;
-                        top: 0;
-                        left: 0;
-                        width: 100%;
-                        height: 100%;
-                        object-fit: cover;
+                        top: 10%;
+                        left: 10%;
+                        width: 80%;
+                        height: 80%;
+                        object-fit: contain;
                         transition: transform 0.3s;
                     }
 
